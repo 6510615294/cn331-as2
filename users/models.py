a2a0d170971284cf35352b7aee23eb02b096f750
Kawin Sangsivarit <6510615021@CN331>
2024-10-04 00:47:17 +0700

    v1.6.2

--- users/models.py
+++ users/models.py
@@ -3,17 +3,22 @@ from django.db import models
 # Create your models here.
 class Subject(models.Model):
     code = models.CharField(max_length=5)
-    subject_name = models.CharField(max_length=64)
-    year = models.CharField(max_length=4)
-    semester = models.CharField(max_length=1)
-    seat = models.CharField(max_length=4)
-    status = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    semester = models.IntegerField()
+    sec = models.CharField(max_length=6)
+    date = models.DateField()
+    teacher = models.CharField(max_length=100)
+    request = models.IntegerField()
+    seat = models.IntegerField()
+    note = models.CharField(max_length=100)
     def __str__(self):
-        return f"{self.code}, {self.subject_name}, {self.year}, {self.semester}, {self.seat}, {self.status}"
+        return f"{self.code}, {self.name}, {self.year}, {self.semester}, {self.sec}, {self.date}, {self.teacher}, {self.request} {self.seat}, {self.note}"
     
 class Student(models.Model):
-    student_code = models.CharField(max_length=10, unique=True)
+    SID = models.CharField(max_length=10, unique=True)
     first = models.CharField(max_length=100)
     last = models.CharField(max_length=100)
+    faculty = models.CharField(max_length=100)
     def __str__(self):
-        return f"{self.student_code} {self.first} {self.last}"
+        return f"{self.SID}, {self.first}, {self.last}, {self.faculty}"
